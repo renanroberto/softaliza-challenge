3a14949cd690eda33b853fa5e0a3d3b0a548c9bb
Renan <renanroberto1@gmail.com>
2020-10-12 12:04:30 -0300

    ecto already validates date and time

--- lib/softaliza_web/controllers/event_controller.ex
+++ lib/softaliza_web/controllers/event_controller.ex
@@ -38,21 +38,7 @@ defmodule SoftalizaWeb.EventController do
   end
 
   def create(conn, params) do
-    start_date = parse_date(params["start_date"])
-    end_date = parse_date(params["end_date"])
-
-    start_hour = parse_time(params["start_hour"])
-    end_hour = parse_time(params["end_hour"])
-
-    event =
-      Map.merge(params, %{
-        "start_date" => start_date,
-        "end_date" => end_date,
-        "start_hour" => start_hour,
-        "end_hour" => end_hour
-      })
-
-    case Events.create_event(event) do
+    case Events.create_event(params) do
       {:ok, user} ->
         conn
         |> put_status(201)
@@ -68,25 +54,6 @@ defmodule SoftalizaWeb.EventController do
         conn
         |> put_status(400)
         |> json(%{status: "error", errors: errors})
-    end
-  end
-
-  # TODO provide better error when fail to parse
-  defp parse_date(nil), do: nil
-
-  defp parse_date(str) do
-    case Date.from_iso8601(str) do
-      {:ok, date} -> date
-      {:error, :invalid_format} -> nil
-    end
-  end
-
-  defp parse_time(nil), do: nil
-
-  defp parse_time(str) do
-    case Time.from_iso8601(str) do
-      {:ok, time} -> time
-      {:error, :invalid_format} -> nil
     end
   end
 end
